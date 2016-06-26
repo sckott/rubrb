82c7cbda92bd4420b86f4e7833a16d38b26739bf
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-26 13:50:57 -0700

    added methods array file

--- lib/rubrb.rb
+++ lib/rubrb.rb
@@ -11,10 +11,38 @@ module Rubrb
   #
   # @example
   #      require 'rubrb'
+  #      Rubrb.test(file: 'tests/testthat/test-config-fxns.R')
   #      Rubrb.test(file: 'config-fxns')
   #      Rubrb.test(file: 'config')
   def self.test(file:)
-    system("Rscript -e \"print('%s')\"" % file)
+    #system("Rscript -e \"print('%s')\"" % file)
+    files = list_files()
+    file = files.guess_file_name(file)
+    system("Rscript -e \"Sys.setenv(NOT_CRAN = 'true'); library(request); library(testthat); test_file('%s')\"" % file)
+  end
+
+  private
+
+  def guess_file_name(x)
+    x.keep_if { |z| !!z.match(/test-/) }
+  end
+
+  def list_files
+    begin
+      files = dir_files('tests/testthat')
+      path = files.keep_if { |z| !!z.match(/test-/) }
+      return path
+    rescue Exception => e
+      puts 'Directory tests/testthat does not exist'
+    end
+  end
+
+  def dir_files(x)
+    Dir.entries(x).select { |entry|
+      !File.directory? File.join(x, entry) and !(entry =='.' || entry == '..')
+    }.map { |z|
+      x + '/' + z
+    }
   end
 
 end
