d285524314ead0d53b28b3a0073860aba6944576
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-26 15:00:24 -0700

    fix utils wrap in pkg class name

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,28 +1,32 @@
 require 'yaml'
 
-def get_pkg_name
-  begin
-    desc = YAML.load_file('DESCRIPTION')
-    return desc['Package']
-  rescue Exception => e
-    puts e
-  end
-end
+module Rubrb
 
-def list_files
-  begin
-    files = dir_files('tests/testthat')
-    path = files.keep_if { |z| !!z.match(/test-/) }
-    return path
-  rescue Exception => e
-    puts 'Directory tests/testthat does not exist'
+  def get_pkg_name
+    begin
+      desc = YAML.load_file('DESCRIPTION')
+      return desc['Package']
+    rescue Exception => e
+      puts e
+    end
   end
-end
 
-def dir_files(x)
-  Dir.entries(x).select { |entry|
-    !File.directory? File.join(x, entry) and !(entry =='.' || entry == '..')
-  }.map { |z|
-    x + '/' + z
-  }
+  def list_files
+    begin
+      files = dir_files('tests/testthat')
+      path = files.keep_if { |z| !!z.match(/test-/) }
+      return path
+    rescue Exception => e
+      puts 'Directory tests/testthat does not exist'
+    end
+  end
+
+  def dir_files(x)
+    Dir.entries(x).select { |entry|
+      !File.directory? File.join(x, entry) and !(entry =='.' || entry == '..')
+    }.map { |z|
+      x + '/' + z
+    }
+  end
+
 end
